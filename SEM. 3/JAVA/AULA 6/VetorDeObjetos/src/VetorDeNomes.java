139b5695c650d505cddd74780d4fb62e9e72a16f
Raphael Eça <raphael2904@gmail.com>
2024-03-22 20:06:11 -0300

    Atualização VetorDeNomes metodos

--- SEM. 3/JAVA/AULA 6/VetorDeObjetos/src/VetorDeNomes.java
+++ SEM. 3/JAVA/AULA 6/VetorDeObjetos/src/VetorDeNomes.java
@@ -5,6 +5,28 @@ public class VetorDeNomes{
     public static void main(String[]args){
 
         Scanner sc = new Scanner(System.in);
+        String[] nomes = criarVetorDeNomes(sc);
+        entrarNomes(nomes, sc);
+        exibirNomes(nomes);
+    }
+
+    private static void exibirNomes(String[] nomes) {
+        System.out.println("Os nomes entrandos são:");
+
+        for (String nome : nomes){
+            System.out.println(nome);
+        }
+    }
+
+    private static void entrarNomes(String[] nomes, Scanner sc) {
+        //Entrar com os nomes
+        for(int i = 0; i< nomes.length; i++){
+            System.out.println("Nome " + i + " de "+ nomes.length);
+            nomes[i] = sc.nextLine();
+        }
+    }
+
+    private static String[] criarVetorDeNomes(Scanner sc) {
         System.out.println("Informe o número de nomes:");
 
         int numNomes = sc.nextInt();
@@ -12,17 +34,6 @@ public class VetorDeNomes{
 
         //Alocar o Vetor
         String[]nomes = new String[numNomes];
-
-        //Entrar com os nomes
-        for(int i = 0; i< nomes.length; i++){
-            System.out.println("Nome " + i + " de "+ nomes.length);
-            nomes[i] = sc.nextLine();
-        }
-
-        System.out.println("Os nomes entrandos são:");
-
-        for (String nome : nomes){
-            System.out.println(nome);
-        }
+        return nomes;
     }
 }
