d678b698291545c54437f5bd357ad962be8c95e8
Raphael Eça <raphael2904@gmail.com>
2024-03-25 12:37:34 -0300

    Update

---
+++ SEM. 1-2/ARDUINO/VARIOS LEDS/varios_leds1.ino
@@ -0,0 +1,71 @@
+// C++ code
+//
+void setup()
+{
+  pinMode(7, OUTPUT);
+  pinMode(6, OUTPUT);
+  pinMode(5, OUTPUT);
+  pinMode(4, OUTPUT);
+}
+
+void loop()
+{
+  digitalWrite(7, HIGH);
+  delay(250); 
+  digitalWrite(7, LOW);
+  digitalWrite(6, HIGH);
+  delay(250);
+  digitalWrite(6, LOW);
+  digitalWrite(5, HIGH);
+  delay(250);
+  digitalWrite(5, LOW);
+  digitalWrite(4, HIGH);
+  delay(250);
+  digitalWrite(4, LOW);
+  delay(250);
+  digitalWrite(4, HIGH); 
+  delay(250);
+  digitalWrite(4, LOW);
+  delay(250);
+  digitalWrite(4, HIGH);
+  delay(250);
+  digitalWrite(4, LOW);
+  digitalWrite(7, HIGH);
+  delay(250);
+  digitalWrite(7, LOW);
+  digitalWrite(6, HIGH);
+  delay(250);
+  digitalWrite(7,HIGH);
+  digitalWrite(6,LOW);
+  delay(250);
+  digitalWrite(7, LOW);
+  digitalWrite(6, HIGH);
+  delay(250);
+  digitalWrite(6, LOW);
+  delay(250);
+  digitalWrite(6, HIGH);
+  delay(250);
+  digitalWrite(6, LOW);
+  delay(250);
+  digitalWrite(6, HIGH);
+  delay(250);
+  digitalWrite(6, LOW);
+  digitalWrite(3, HIGH);
+  delay(250);
+  digitalWrite(3,LOW);
+  digitalWrite(2,HIGH);
+  delay(250);  
+  digitalWrite(2,LOW);
+  digitalWrite(8, HIGH);
+  delay(250);
+  digitalWrite(8,LOW);
+  delay(250);
+  digitalWrite(8,HIGH);
+  delay(250);
+  digitalWrite(8,LOW);
+  delay(250);
+  digitalWrite(8,HIGH);
+  delay(250);
+  digitalWrite(8,LOW);
+  delay(3000);
+}
