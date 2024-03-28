01475618f2d2a0649dd3fb3daadf8e6935fa0f1b
Raphael Eça <raphael2904@gmail.com>
2024-03-27 21:20:51 -0300

    up

--- SEM. 3/ANDROID JAVA/app/src/main/java/br/com/teste/planetas/MainActivity.java
+++ SEM. 3/ANDROID JAVA/app/src/main/java/br/com/teste/planetas/MainActivity.java
@@ -1,8 +1,10 @@
 package br.com.teste.planetas;
 
 import android.os.Bundle;
+import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.Spinner;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -32,6 +34,23 @@ public class MainActivity extends AppCompatActivity {
         adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         Spinner spinner = (Spinner) findViewById(R.id.spinnerPlanetas);
         spinner.setAdapter(adapter);
+    }
+
+    public void showSelecionados(View v){
+        Spinner planetasSpinner = findViewById(R.id.spinnerPlanetas);
+        Spinner timesASpinner = findViewById(R.id.spinnerTimes);
+
+        Object planetaObj = planetasSpinner.getSelectedItem();
+        Object timeAObj = timesASpinner.getSelectedItem();
+
+        String planeta = planetaObj.toString();
+        String timeA = timeAObj.toString();
+
+        String messagem = "Planeta Selecionado:" + planeta
+                + " Time Selecionado: " +timeA;
+
+        Toast.makeText(this, messagem, Toast.LENGTH_LONG)
+                .show();
     }
     /*public void initSpinner(){
         ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this, R.array.timesSerieA,
