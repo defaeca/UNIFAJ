1ca812fbbef826edbfcb0ebd36e0821047e16502
Raphael Eça <raphael2904@gmail.com>
2024-04-19 21:16:36 -0300

    up

---
+++ SEM. 3/JAVA/AulaServico/src/main/java/br/edu/unifaj/poo/AulaServico/AulaServicoApplication.java
@@ -0,0 +1,14 @@
+package br.edu.unifaj.poo.AulaServico;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AulaServicoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AulaServicoApplication.class, args);
+	}
+
+}
+//http://localhost:63342/AulaServico/SEM.%203/JAVA/AulaServico/src/main/resources/static/index.html?_ijt=pnlmt01m0l9nn3gaevn3do379j&_ij_reload=RELOAD_ON_SAVE
---
+++ SEM. 3/JAVA/AulaServico/src/test/java/br/edu/unifaj/poo/AulaServico/AulaServicoApplicationTests.java
@@ -0,0 +1,13 @@
+package br.edu.unifaj.poo.AulaServico;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AulaServicoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
